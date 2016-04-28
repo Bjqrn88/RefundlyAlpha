c23773c9662d7b87a41921d9f618ba9147da4645
kangarooooooo <eirikonielsen@gmail.com>
2016-04-28 17:01:40 +0200

    Nu med en riktig shitty singleton

---
+++ app/src/main/java/com/macbear/refundlyalpha/Singleton.java
@@ -0,0 +1,12 @@
+package com.macbear.refundlyalpha;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+
+/**
+ * Created by Eier on 28.04.2016.
+ */
+public class Singleton {
+    //public static AccountManager manager = (AccountManager) getSystemService(ACCOUNT_SERVICE);
+    //Account[] list = manager.getAccounts();
+}
